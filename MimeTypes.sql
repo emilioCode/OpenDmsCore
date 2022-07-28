8d9b63e915765f6d6c88f3d47f6da4cbf839c4f2
Manuel E Mendez <emilio_mem@hotmail.com>
2022-07-28 10:44:20 -0400

    Clean Architecture

---
+++ MimeTypes.sql
@@ -0,0 +1,152 @@
+﻿USE OPEN_DMS;
+
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("aac","AAC audio", "audio/aac"); 
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("abw","AbiWord document", "application/x-abiword");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("arc","Archive document (multiple files embedded)", "application/octet-stream");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("avif","AVIF image", "image/avif");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("avi","AVI: Audio Video Interleave", "video/x-msvideo");            
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("azw","Amazon Kindle eBook format", "application/vnd.amazon.ebook");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("bin","Any kind of binary data", "application/octet-stream");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("bmp","Windows OS/2 Bitmap Graphics","image/bmp");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("bz","BZip archive	", "application/x-bzip");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("bz2","BZip2 archive", "application/x-bzip2");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("cda","CD audio","application/x-cdf");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("csh","C-Shell script", "application/x-csh");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("css","Cascading Style Sheets (CSS)", "text/css");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("csv","Comma-separated values (CSV)", "text/csv");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  	
+("doc","Microsoft Word", "application/msword");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  	
+("docx","Microsoft Word (OpenXML)", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");            
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("eot","MS Embedded OpenType fonts", "application/vnd.ms-fontobject");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("epub","Electronic publication (EPUB)", "application/epub+zip");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("gz","GZip Compressed Archive", "application/gzip");            
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("gif","Graphics Interchange Format (GIF)", "image/gif");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("htm","HyperText Markup Language (HTML)", "text/html");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("html","HyperText Markup Language (HTML)", "text/html");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("ico","Icon format", "image/vnd.microsoft.icon");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("ics","iCalendar format", "text/calendar");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("jar","Java Archive (JAR)", "application/java-archive");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  	
+("jpg","JPEG images", "image/jpeg");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("jpeg","JPEG images", "image/jpeg");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("js","JavaScript (ECMAScript)", "text/javascript");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("json","JSON format", "application/json");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("jsonld","JSON-LD format", "application/ld+json");            
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("mid","Musical Instrument Digital Interface (MIDI)", "audio/midi");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("midi","Musical Instrument Digital Interface (MIDI)", "audio/midi");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("mjs","JavaScript module", "text/javascript");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("mp3","MP3 audio", "audio/mpeg");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("mp4","MP4 video", "video/mp4");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("mpeg","MPEG Video", "video/mpeg");			
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("mpkg","Apple Installer Package", "application/vnd.apple.installer+xml");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("odp","OpenDocument presentation document", "application/vnd.oasis.opendocument.presentation");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("ods","OpenDocument spreadsheet document", "application/vnd.oasis.opendocument.spreadsheet");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("odt","OpenDocument text document", "application/vnd.oasis.opendocument.text");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("oga","OGG audio", "audio/ogg");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("ogv","OGG video", "video/ogg");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("ogx","OGG", "application/ogg");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("opus","Opus audio", "audio/opus");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("otf","OpenType font","font/otf");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("png","Portable Network Graphics","image/png");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("pdf","Adobe Portable Document Format (PDF)", "application/pdf");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("php","Hypertext Preprocessor (Personal Home Page)", "application/x-httpd-php");            
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("ppt","Microsoft PowerPoint", "application/vnd.ms-powerpoint");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("pptx","Microsoft PowerPoint (OpenXML)", "application/vnd.openxmlformats-officedocument.presentationml.presentation");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("rar","RAR archive", "application/vnd.rar");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("rtf","Rich Text Format (RTF)", "application/rtf");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("sh","Bourne shell script", "application/x-sh");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("svg","Scalable Vector Graphics (SVG)", "image/svg+xml");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("swf","Small web format (SWF) or Adobe Flash document", "application/x-shockwave-flash");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("tar","Tape Archive (TAR)", "application/x-tar");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("tif","Tagged Image File Format (TIFF)", "image/tiff");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("tif","Tagged Image File Format (TIFF)", "image/tiff");            
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("ts","MPEG transport stream", "video/mp2t");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("ttf","TrueType Font", "font/ttf");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("txt","Text, (generally ASCII or ISO 8859-n)", "text/plain");           
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("vsd","Microsft Visio", "application/vnd.visio");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("wav","Waveform Audio Format", "audio/wav");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("weba","WEBM audio", "audio/webm");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("webm","WEBM video", "video/webm");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("webp","WEBP image", "image/webp");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("woff","Web Open Font Format (WOFF)", "font/woff");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("woff2","Web Open Font Format (WOFF)", "font/woff2");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("xhtml","XHTML", "application/xhtml+xml");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("xls","Microsoft Excel", "application/vnd.ms-excel");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("xlsx","Microsoft Excel (OpenXML)", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");            
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("xml","XML", "application/xml");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES  
+("xul","XUL (XML User Interface Language)", "application/vnd.mozilla.xul+xml");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("zip","ZIP archive", "application/zip");
+INSERT INTO open_dms.mimetypes (extension,kinf_of_document,mime_type) VALUES 
+("7z","7-zip archive", "application/x-7z-compressed");
---
+++ OPEN_DMS.sql
@@ -0,0 +1,63 @@
+﻿CREATE DATABASE OPEN_DMS;
+
+USE OPEN_DMS;
+
+CREATE TABLE ENTITIES(
+	id int NOT NULL AUTO_INCREMENT,
+    entityName varchar(50) NOT NULL,
+    disabled boolean NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE TEAMS(
+	id int NOT NULL AUTO_INCREMENT,
+    entityId int NOT NULL,
+    teamName varchar(50) NOT NULL,
+    pathRoot text NOT NULL,
+	telephoneNumber varchar(50) NULL,
+    hostName varchar(30) NULL,
+    portNumber int NULL,
+    email varchar(50) NULL,
+    pass varchar(50) NULL,
+    disabled boolean NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE USERS(
+	id int NOT NULL AUTO_INCREMENT,
+    completeName varchar(255) NOT NULL,
+    description text NULL,
+    userAccount varchar(20) NOT NULL,
+    userPassword varchar(80) NOT NULL,
+    teamId int NULL,
+    entityId int NOT NULL,
+    accessLevel varchar(20) NULL,
+	createdDate date NOT NULL,
+    expirationDate date NOT NULL,
+    disabled boolean NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE DOCUMENTS(
+    id int NOT NULL AUTO_INCREMENT,
+    fileName varchar(50) NOT NULL,
+    extension varchar(5) NOT NULL,
+    size float NOT NULL,
+    teamId int NOT NULL,
+    entityId int NOT NULL,
+    insertionDate date NOT NULL,
+    pathAlternative text NULL,
+    commentDetail text NULL,
+    distinctDetail text NULL,
+    idUser int NOT NULL,
+    disabled boolean NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE MIMEtypes (
+	id int NOT NULL AUTO_INCREMENT,
+    extension varchar(6) NOT NULL,
+    kinf_of_document text NULL,
+    mime_type varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+);
